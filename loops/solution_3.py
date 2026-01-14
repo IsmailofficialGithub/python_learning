4d046f108fbfa3824a44eaa7cef6d463ac2b0f17
IsmailofficialGithub <ismail.official295@gmail.com>
2026-01-14 17:01:46 +0500

    Add multiplication table script that skips the fifth iteration

---
+++ loops/solution_3.py
@@ -0,0 +1,9 @@
+print('Print the multiplication table for a given number up to 10 , but skip the fifth iternation')
+
+#solution
+
+number=4
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(number , 'x' , i , '=', number*i)
