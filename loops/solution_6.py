654720290bcdf21433019403af50bd6e20e288bf
IsmailofficialGithub <ismail.official295@gmail.com>
2026-01-19 17:21:45 +0500

    Add script to compute the factorial of a number using a while loop

---
+++ loops/solution_6.py
@@ -0,0 +1,11 @@
+print('Compute the factorial of a number using a while loop.')
+
+# 5! = 5*4*3*2*1
+number= 5
+factorial=1
+
+while number> 0:
+    factorial= factorial * number
+    number =number-1
+
+print("Factorial Value : ", factorial)
