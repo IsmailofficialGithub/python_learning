da9ae612a3b87280d9d180df5d2dc9fabf2b38e0
IsmailofficialGithub <ismail.official295@gmail.com>
2026-01-19 17:52:37 +0500

    Add script to check if a number is prime

---
+++ loops/solution_8.py
@@ -0,0 +1,12 @@
+print('Prime number checker')
+number =11
+
+is_prime= True
+
+if number > 1:
+    for i in range(2, number):
+        if(number % i) ==0:
+            is_prime=False
+            break
+
+print(is_prime)
