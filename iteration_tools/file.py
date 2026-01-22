220a10eccc4def7cac637c08a94cf50b73de566f
Ismail Abbasi <ismail.official295@gmail.com>
2026-01-22 21:30:01 +0500

    Add initial implementation of file.py with user print statements

---
+++ iteration_tools/file.py
@@ -0,0 +1,16 @@
+import time
+user='Ismail abbasi'
+print('user is ',user)
+print ('this is file from file.py')
+
+    # in terminal
+
+
+# >>> f.__next__()
+# 'import time\n'
+
+
+# for line in open("file.py"):
+# ...     print(line)   
+#    print(line,end='')
+
