85b430d9abd13c005993e2575d00a499929afe3f
Ismail Abbasi <ismail.official295@gmail.com>
2026-01-22 22:07:02 +0500

    Add implementation of mul_of_num function to multiply two parameters, including string handling

---
+++ Functions/solution3.py
@@ -0,0 +1,9 @@
+print("Function that take multiple parameters and return its multiplicational result also accept strings")
+
+def mul_of_num(value1,value2):
+    
+    return value1*value2
+
+result=mul_of_num('give me star',4)
+print(result)
+
